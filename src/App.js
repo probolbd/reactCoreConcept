161cbc4ec0027f5fa72c641ca09c0b5d1a5f2e44
Tarekul Islam <probolbd@yahoo.com>
2021-02-26 18:26:34 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -9,13 +9,13 @@ function App() {
       <Tarekul></Tarekul>
       <Tarekul></Tarekul>
       <Counter></Counter>
+      <Users></Users>
       </header>
     </div>
   );
 }
 function Tarekul() {
   return (<div><h1>Tarekul Name</h1>
-  
     <h2>soma</h2>
     </div>)
 }
@@ -26,6 +26,35 @@ function Counter() {
       <h1>Count: {saba}</h1>
       <button onClick={() => setCount(saba+1)}>Increase</button>
       <button onClick={() => setCount(saba-1)}>Decrease</button>
+    </div>
+  )
+}
+function Users() {
+  const styleClass={
+    border:'2px solid white',
+    backgroundColor:'gray',
+    padding:'20px',
+    margin:'10px',
+    listStyle:'none',
+    float:'left',
+    width:'230px',
+    height:'100px',
+    borderRadius:'20px',
+    boxShadow:'5px 5px 10px yellow',
+    fontSize:'20px'
+  }
+  const [users, setUsers]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[])
+  return (
+    <div >
+      <h3>Dynamic site: {users.length}</h3>
+      <ul>
+        {users.map(user => <li style={styleClass}>{user.name} <br/> {user.email} <br/> {user.phone}</li>)}
+      </ul>
     </div>
   )
 }
